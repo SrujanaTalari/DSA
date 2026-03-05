dc2d22030f91aa95b06c0e00a23e3e7e1ef9d8ff
SrujanaTalari <srujanatalari65@gmail.com>
2026-03-05 20:11:26 +0530

    arr1.java

---
+++ arr1.java
@@ -0,0 +1,10 @@
+class arr1 {
+    public static void main(String[] args) {
+       // int[] numbers;
+       // numbers=new int[5];
+       // numbers=new int[] {1,2,3,4,5};
+       int [] numbers=new int[] {7,5,4,3,2};
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[4]);        }
+    }
